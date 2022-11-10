95ff146866cb7f975704ccfe8fc89feccb6a5b00
mohammedyusufazeez <yusufmech96@gmail.com>
2022-11-10 08:01:00 +0530

    My Task

---
+++ src/org/company/companyInfo.java
@@ -0,0 +1,25 @@
+package org.company;
+
+public class companyInfo {
+	private void companyName() {
+		// TODO Auto-generated method stub
+System.out.println("Company Name : AS Enterprises");
+	}
+private void companyId() {
+	// TODO Auto-generated method stub
+System.out.println("Company Id : 786");
+}
+private void companyAddress() {
+	// TODO Auto-generated method stub
+System.out.println("Company Address : Madurai");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+companyInfo a = new companyInfo();
+a.companyName();
+a.companyAddress();
+a.companyId();
+
+	}
+
+}
